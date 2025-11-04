4f94f7e910c16de39aef1ee083a7ce3ef73a13bc
LucyLenn <ov.lclnn@gmail.com>
2025-11-04 22:16:27 +0200

    removed cycles

--- homework2/main.dart
+++ homework2/main.dart
@@ -6,98 +6,74 @@ void main() {
 }
 
 void convertGrade() {
-  while (true) {
-    stdout.write('Введіть ваш бал (або "exit" для виходу): ');
-    String? input = stdin.readLineSync();
+  stdout.write('Введіть ваш бал: ');
+  String? input = stdin.readLineSync();
 
-    if (input == null || input.toLowerCase() == 'exit') {
-      break;
-    }
+  int? score = int.tryParse(input ?? '');
 
-    int? score = int.tryParse(input);
-
-    if (score == null || score < 0 || score > 100) {
-      print('Помилка даних. Введіть невід\'ємне ціле число від 0 до 100.');
-      continue;
-    }
-
-    if (score >= 90) {
-      print('Ваша оцінка: A');
-    } else if (score < 90 && score >= 80) {
-      print('Ваша оцінка: B');
-    } else if (score < 80 && score >= 70) {
-      print('Ваша оцінка: C');
-    } else if (score < 70 && score >= 60) {
-      print('Ваша оцінка: D');
-    } else {
-      print('Ваша оцінка: F');
-    }
+  if (score == null || score < 0 || score > 100) {
+    print('Помилка даних. Введіть невід\'ємне ціле число від 0 до 100.');
+    return;
   }
 
-  print('Програма завершена.');
+  if (score >= 90) {
+    print('Ваша оцінка: A');
+  } else if (score < 90 && score >= 80) {
+    print('Ваша оцінка: B');
+  } else if (score < 80 && score >= 70) {
+    print('Ваша оцінка: C');
+  } else if (score < 70 && score >= 60) {
+    print('Ваша оцінка: D');
+  } else {
+    print('Ваша оцінка: F');
+  }
 }
 
 void guessNumber() {
   int randomInt = Random().nextInt(100) + 1;
-
   int? userNumber;
 
   print('Гра "Вгадай число від 1 до 100":');
 
   while (randomInt != userNumber) {
-    stdout.write('Введіть число (або "exit" для виходу): ');
+    stdout.write('Введіть число: ');
     String? input = stdin.readLineSync();
 
-    if (input == null || input.toLowerCase() == 'exit') {
-      break;
-    }
-
-    int? userNumber = int.tryParse(input);
+    int? userNumber = int.tryParse(input ?? '');
     if (userNumber == null || userNumber < 1) {
       print('Помилка даних. Введіть невід\'ємне ціле число від 1 до 100.');
-      continue;
+      return;
     }
 
     if (randomInt == userNumber) {
       print('Ви вгадали.');
-      break;
+      return;
     }
 
     String word = (randomInt > userNumber) ? 'більше' : 'меньше';
     print('Ви не вгадали. Підказка: загадане число $word.');
   }
-
-  print('Програма завершена.');
 }
 
 void getFactorial() {
-  while (true) {
-    stdout.write(
-        'Введіть число для розрахунку його факторіалу (або "exit" для виходу): ');
-    String? input = stdin.readLineSync();
+  stdout.write('Введіть число для розрахунку його факторіалу: ');
+  String? input = stdin.readLineSync();
 
-    if (input == null || input.toLowerCase() == 'exit') {
-      break;
-    }
+  int? number = int.tryParse(input ?? '');
 
-    int? number = int.tryParse(input);
-
-    if (number == null || number < 0) {
-      print('Помилка даних. Введіть невід\'ємне ціле число.');
-      continue;
-    }
-
-    int i = 1;
-    int factorial = 1;
-    while (i <= number) {
-      factorial = factorial * i;
-      i++;
-    }
-
-    print('Результат: $number! = $factorial');
+  if (number == null || number < 0) {
+    print('Помилка даних. Введіть невід\'ємне ціле число.');
+    return;
   }
 
-  print('Програма завершена.');
+  int i = 1;
+  int factorial = 1;
+  while (i <= number) {
+    factorial = factorial * i;
+    i++;
+  }
+
+  print('Результат: $number! = $factorial');
 }
 
 void convertTemperature() {
@@ -109,40 +85,32 @@ void convertTemperature() {
     return (temperature - 32) / 1.8;
   }
 
-  while (true) {
-    stdout.write('Введіть температуру (або "exit" для виходу): ');
-    String? input = stdin.readLineSync();
+  stdout.write('Введіть температуру: ');
+  String? input = stdin.readLineSync();
 
-    if (input == null || input.toLowerCase() == 'exit') {
-      break;
-    }
+  double? temperature = double.tryParse(input ?? '');
 
-    double? temperature = double.tryParse(input);
-
-    if (temperature == null) {
-      print('Помилка даних. Введіть число.');
-      continue;
-    }
-
-    print('Оберіть систему конвертації:');
-    print('1 - з Цельсія у Фаренгейти');
-    print('2 - з Фаренгейтів у Цельсії');
-    String? choice = stdin.readLineSync();
-
-    switch (choice) {
-      case '1':
-        double result = celsiusToFahrenheit(temperature);
-        print('$temperature°C = ${result.toStringAsFixed(2)}°F');
-      case '2':
-        double result = fahrenheitToCelsius(temperature);
-        print('$temperature°F = ${result.toStringAsFixed(2)}°C');
-      default:
-        print('Невірний вибір, спробуйте ще раз.');
-        continue;
-    }
+  if (temperature == null) {
+    print('Помилка даних. Введіть число.');
+    return;
   }
 
-  print('Програма завершена.');
+  print('Оберіть систему конвертації:');
+  print('1 - з Цельсія у Фаренгейти');
+  print('2 - з Фаренгейтів у Цельсії');
+  String? choice = stdin.readLineSync();
+
+  switch (choice) {
+    case '1':
+      double result = celsiusToFahrenheit(temperature);
+      print('$temperature°C = ${result.toStringAsFixed(2)}°F');
+    case '2':
+      double result = fahrenheitToCelsius(temperature);
+      print('$temperature°F = ${result.toStringAsFixed(2)}°C');
+    default:
+      print('Невірний вибір, спробуйте ще раз.');
+      return;
+  }
 }
 
 void calculate() {
@@ -161,50 +129,46 @@ void calculate() {
     return num1 / num2;
   }
 
-  while (true) {
-    stdout.write('Введіть перше число (або "exit" для виходу): ');
-    String? input1 = stdin.readLineSync();
+  stdout.write('Введіть перше число: ');
+  String? input1 = stdin.readLineSync();
 
-    if (input1 == null || input1.toLowerCase() == 'exit') {
-      break;
-    }
+  double? num1 = double.tryParse(input1 ?? '');
 
-    stdout.write('Введіть друге число: ');
-    String? input2 = stdin.readLineSync();
-
-    if (input2 == null) break;
-
-    double? num1 = double.tryParse(input1);
-    double? num2 = double.tryParse(input2);
-
-    if (num1 == null || num2 == null) {
-      print('Помилка даних. Введіть коректні числа.');
-      continue;
-    }
-
-    stdout.write('Оберіть операцію (+, -, *, /): ');
-    String? operation = stdin.readLineSync();
-
-    double? result;
-
-    switch (operation) {
-      case '+':
-        result = add(num1, num2);
-      case '-':
-        result = subtract(num1, num2);
-      case '*':
-        result = multiply(num1, num2);
-      case '/':
-        result = divide(num1, num2);
-      default:
-        print('Невідома операція, спробуйте ще раз.');
-        continue;
-    }
-
-    if (result != null) {
-      print('Результат: $num1 $operation $num2 = $result');
-    }
+  if (num1 == null) {
+    print('Помилка даних. Введіть число.');
+    return;
   }
 
-  print('Програма завершена.');
+  stdout.write('Введіть друге число: ');
+  String? input2 = stdin.readLineSync();
+
+  double? num2 = double.tryParse(input2 ?? '');
+
+  if (num2 == null) {
+    print('Помилка даних. Введіть число.');
+    return;
+  }
+
+  stdout.write('Оберіть операцію (+, -, *, /): ');
+  String? operation = stdin.readLineSync();
+
+  double? result;
+
+  switch (operation) {
+    case '+':
+      result = add(num1, num2);
+    case '-':
+      result = subtract(num1, num2);
+    case '*':
+      result = multiply(num1, num2);
+    case '/':
+      result = divide(num1, num2);
+    default:
+      print('Невідома операція, спробуйте ще раз.');
+      return;
+  }
+
+  if (result != null) {
+    print('Результат: $num1 $operation $num2 = $result');
+  }
 }
